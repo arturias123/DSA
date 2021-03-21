23305a4e5b4a34d60bcdb9f6cd7cf4ab7f583318
arturias123 <52829156+arturias123@users.noreply.github.com>
2021-03-21 14:47:59 +0700

    Add files via upload

--- VM.cpp
+++ VM.cpp
@@ -1,16 +1,22 @@
 #include "VM.h"
 // regex for instruction
 regex twoOps("(\\w+)\\s(\\w+),\\s(\\S+)$");
-regex oneOp("(\\w+)\\s(\\w+)");
-regex noOp("(\\w+)");
-regex arith("(Add|Minus|Div|Mul)\\sR([1-9]|1[0-5]),\\s(\\S+)");
-regex cmp("Cmp(EQ|NE|LT|LE|GT|GE)\\sR([1-9]|1[0-5]),\\s(\\S+)");
-regex log1("(And|Or)\\sR([1-9]|1[0-5]),\\s(\\S+)");
-regex nein("Not\\sR([1-9]|1[0-5])");
-regex ls("(Move|Load|Store)\\sR([1-9]|1[0-5]),\\s(\\S+)");
-regex seq1("Return|Halt");
-regex seq2("(Jump|Call)\\s(\\S+)");
-regex seq3("JumpIf\sR([1-9]|1[0-5]),\\s(\\S+)");
+regex oneOp("(\\w+)\\s(\\w+)$");
+regex noOp("(\\w+)$");
+regex arith("(Add|Minus|Div|Mul)\\sR([1-9]|1[0-5]),\\s(\\d+|\\d+[.]\\d+)$");
+regex arithReg("(Add|Minus|Div|Mul)\\sR([1-9]|1[0-5]),\\sR([1-9]|1[0-5])$");
+regex cmp("Cmp(EQ|NE|LT|LE|GT|GE)\\sR([1-9]|1[0-5]),\\s(\\S+)$");
+regex log1("(And|Or)\\sR([1-9]|1[0-5]),\\s(\\S+)$");
+regex nein("Not\\sR([1-9]|1[0-5])$");
+regex ls("(Move|Load|Store)\\sR([1-9]|1[0-5]),\\s(\\S+)$");
+regex seq1("Return|Halt$");
+regex seq2("(Jump|Call)\\s(\\S+)$");
+regex seq3("JumpIf\sR([1-9]|1[0-5]),\\s(\\S+)$");
+regex boolean("true|false");
+regex address("(\\d+[A]$");
+regex integer("\\d+");
+regex flOat("\\d+[.]\\d+");
+
 
 void VM::run(string filename)
 {
@@ -26,10 +32,13 @@ void VM::run(string filename)
 		ins[i] = myText;
 		if (regex_match(ins[i], twoOps)) {
 			if (regex_match(ins[i], arith)) {
-
+				calculate(ins[i]);
+			}
+			else if (regex_match(ins[i], arithReg)) {
+				calculate(ins[i]);
 			}
 			else if (regex_match(ins[i], cmp)) {
-
+				compare(ins[i]);
 			}
 			else if (regex_match(ins[i], log1)) {
 
@@ -65,3 +74,108 @@ void VM::run(string filename)
 	// Close the file
 	myReadFile.close();
 }
+
+void VM::calculate(string input) {
+	smatch s;
+	string a, b;
+	int dest, src;
+	bool ints = 0;
+	if (regex_match(input, s, arith)) {
+		a = R[stoi(s[2])];	// register ID
+		b = s[3];			// value
+	}
+	else if (regex_match(input, s, arithReg)) {
+		a = R[stoi(s[2])];	
+		b = R[stoi(s[3])];	// register ID
+	}
+	if (regex_match(a, integer) && regex_match(b, integer)) {
+		ints = 1;
+	}
+	else ints = 0;
+	if (regex_match(a, address) || regex_match(a, boolean) || regex_match(b, address) || regex_match(b, boolean)) {
+		throw TypeMismatch(i);
+	}
+	if (s[1] == "Add") {
+		if (ints == 1) {
+			R[stoi(s[2])] = to_string(stoi(a) + stoi(b));
+		}
+		else {
+			R[stoi(s[2])] = to_string(stod(a) + stod(b)); // assign result to register
+		}
+	}
+	else if (s[1] == "Minus") {
+		if (ints == 1) {
+			R[stoi(s[2])] = to_string(stoi(a) - stoi(b));
+		}
+		else {
+			R[stoi(s[2])] = to_string(stod(a) - stod(b)); 
+		}
+	}
+	else if (s[1] == "Div") {
+		if (stod(b) == 0) {
+			throw DivideByZero(i);
+		}
+		else {
+			if (ints == 1) {
+				R[stoi(s[2])] = to_string(stoi(a) / stoi(b));
+			}
+			else {
+				R[stoi(s[2])] = to_string(stod(a) / stod(b));
+			}
+		}
+	}
+	else if (s[1] == "Mul") {
+		if (ints == 1) {
+			R[stoi(s[2])] = to_string(stoi(a) * stoi(b));
+		}
+		else {
+			R[stoi(s[2])] = to_string(stod(a) * stod(b));
+		}
+	}
+}
+
+void VM::compare(string input) {
+	smatch s;
+	bool res = 0;
+	string a, b;
+	if (regex_match(input, s, cmp)) {
+		a = R[stoi(s[2])]; // dest
+		b = s[3];		   // src
+	}
+	if (regex_match(a, address) || regex_match(b, address)) {
+		throw TypeMismatch(i);
+	}
+	if (regex_match(a, boolean) && !regex_match(b, boolean)) {
+		throw TypeMismatch(i);
+	}
+	if (regex_match(b, boolean) && !regex_match(a, boolean)) {
+		throw TypeMismatch(i);
+	}
+	if (s[1] == "EQ") {
+		res = (a == b);
+	}
+	else if (s[1] == "NE") {
+		res = (a != b);
+	}
+	else if (s[1] == "LT") {
+		res = (a < b);
+	}
+	else if (s[1] == "LE") {
+		res = (a <= b);
+	}
+	else if (s[1] == "GT") {
+		res = (a > b);
+	}
+	else if (s[1] == "GE") {
+		res = (a >= b);
+	}
+	if (res == 1) {
+		R[stoi(s[2])] = "true";
+	}
+	else {
+		R[stoi(s[2])] = "false";
+	}
+}
+
+//stod(s[3])
+//stod(R[stoi(s[3]])
--- VM.h
+++ VM.h
@@ -7,9 +7,14 @@
 class VM
 {
 public:
-  VM(){}
+	VM() {
+		fill_n(R, 16, "0");
+	}
   string ins[65536];
+  string R[16];
   int i = 0;
   void run(string filename);
+  void calculate(string input);
+  void compare(string input);
 };
 #endif
